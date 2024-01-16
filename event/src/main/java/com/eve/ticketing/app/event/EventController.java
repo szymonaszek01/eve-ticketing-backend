096dd2370cfb174eb39e301b681b84ea435847ae
szymonaszek01 <77828609+szymonaszek01@users.noreply.github.com>
2024-01-16 01:04:51 +0100

    [Event Microservice] Implementing controller layer

---
+++ event/src/main/java/com/eve/ticketing/app/event/EventController.java
@@ -0,0 +1,56 @@
+package com.eve.ticketing.app.event;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@Tag(name = "Event", description = "Event management APIs")
+@RequestMapping("/api/v1/event")
+@RequiredArgsConstructor
+@RestController
+public class EventController {
+
+    private final EventServiceImpl eventService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createEvent(@RequestBody Event event) {
+        try {
+            eventService.createEvent(event);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (EventProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable long id) {
+        try {
+            Event event = eventService.getEventById(id);
+            return new ResponseEntity<>(event, HttpStatus.OK);
+        } catch (EventProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<Event>> getEventList(@RequestParam(value = "page") int page,
+                                                    @RequestParam(value = "size") int size,
+                                                    EventFilterDto eventFilterDto) {
+        Page<Event> eventPage = eventService.getEventList(page, size, eventFilterDto);
+        return new ResponseEntity<>(eventPage, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<?> deleteEventById(@PathVariable long id) {
+        try {
+            eventService.deleteEventById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (EventProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+}
