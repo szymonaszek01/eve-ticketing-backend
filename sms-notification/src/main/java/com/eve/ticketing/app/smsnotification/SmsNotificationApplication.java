41a4e64796af09664dd366f53f66341cd9c942fe
szymonaszek01 <77828609+szymonaszek01@users.noreply.github.com>
2024-01-26 22:28:41 +0100

    [Sms Notification Microservice] Adding banner and application

---
+++ sms-notification/src/main/java/com/eve/ticketing/app/smsnotification/SmsNotificationApplication.java
@@ -0,0 +1,14 @@
+package com.eve.ticketing.app.smsnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SmsNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsNotificationApplication.class, args);
+    }
+}
